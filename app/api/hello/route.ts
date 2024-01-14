0055d36597741a761bd993ea4bb306b53fbd5f9d
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-01-14 23:17:50 +0500

    Refactor GET function in hello route.ts to return an array of data

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,10 +1,38 @@
-import { NextResponse , NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest){
-    let dati = {
-         name: "Abbas",
-         age: 12,
-         email: "syedabbasali1998.aa@gmail"
+export async function GET(request: NextRequest) {
+    let dati = [
+        {
+            name: "Abbas",
+            age: 12,
+            email: "syedabbasali1998.aa@gmail"
+        },
+        {
+            name: "John",
+            age: 30,
+            email: "john.doe@example.com"
+        },
+        {
+            name: "Emily",
+            age: 22,
+            email: "emily.jones@example.com"
+        },
+        {
+            name: "Alex",
+            age: 28,
+            email: "alex.smith@example.com"
+        },
+        {
+            name: "Sophia",
+            age: 35,
+            email: "sophia.wilson@example.com"
+        },
+        {
+            name: "Daniel",
+            age: 26,
+            email: "daniel.brown@example.com"
         }
-    return NextResponse.json({message: "Api chal gai" , dati})
+    ];
+
+    return NextResponse.json({ message: "Api chal gai", dati });
 }
