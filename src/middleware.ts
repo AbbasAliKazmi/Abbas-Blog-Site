072dad10beaae3ac07c4f7a94aceeb640aac2451
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-01-20 03:31:44 +0500

    1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextResponse } from "next/server";
 
-const middleware = (req: Request, res: Response, next: NextFunction) => {
-    // Middleware logic goes here
-    // ...
-    next(); // Call next() to pass control to the next middleware or route handler
+export function middleware(){
+    return NextResponse.json({
+        hello:"middleware"
+    })
 };
 
-export default middleware;
+;
