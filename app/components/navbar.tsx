02d4245e985644b1c7d49f57f93e1f2ee0b46de3
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-01-30 00:00:11 +0500

    Add user authentication to Navbar component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
+import { useUser } from "@clerk/nextjs";
 
 export default function Navbar() {
+    const {user, isLoaded} = useUser();
     return (
         <div className="w-full relative flex items-center max-w-2xl mx-auto px-4 py-5 justify-between">
            <Link href="/" className="font-bold text-3xl">
